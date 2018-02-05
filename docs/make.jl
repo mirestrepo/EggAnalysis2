ac35db5b3fff3db304cca59a23df9a1661c957f7
Isabel Restrepo (pencil) <isabelrestre@gmail.com>
2018-02-05 11:58:20 -0500

    Add dependencies for docs

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,7 @@ using Documenter, EggAnalysis2
 makedocs()
 
 deploydocs(
+    deps   = Deps.pip("mkdocs", "python-markdown-math", "mkdocs-material"),    
     repo = "https://github.com/mirestrepo/EggAnalysis2.git",
     julia  = "0.6",
     osname = "linux"
