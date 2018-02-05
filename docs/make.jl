2fda8d8253d693d23037a4ac7e1681c6a38267aa
Isabel Restrepo (pencil) <isabelrestre@gmail.com>
2018-02-05 11:56:33 -0500

    Change docs build

--- docs/make.jl
+++ docs/make.jl
@@ -3,5 +3,7 @@ using Documenter, EggAnalysis2
 makedocs()
 
 deploydocs(
-    repo = "https://github.com/mirestrepo/EggAnalysis2.git"
+    repo = "https://github.com/mirestrepo/EggAnalysis2.git",
+    julia  = "0.6",
+    osname = "linux"
 )
